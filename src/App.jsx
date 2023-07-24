629031822e0f2d930a27a102a88dc1e313b2d64b
chansenhyggedev <chansen.hyggedev@gmail.com>
2023-07-24 12:56:18 -0700

    padding to body

--- src/App.jsx
+++ src/App.jsx
@@ -22,17 +22,17 @@ export default function App() {
   }
 
   return (
-    <div className='min-h-screen w-full grid place-items-center'>
+    <div className='grid w-full min-h-screen p-5 place-items-center'>
       {!submitted ? (
-        <div className="max-w-sm w-full h-fit py-7 rounded-2xl px-6 space-y-6 bg-gradient-to-t from-gray-900 to-gray-800">
-        <div className="w-full flex">
-          <div className='h-12 w-12 rounded-full bg-gray-700/50 grid place-items-center'>
+        <div className="w-full max-w-sm px-6 space-y-6 h-fit py-7 rounded-2xl bg-gradient-to-t from-gray-900 to-gray-800">
+        <div className="flex w-full">
+          <div className='grid w-12 h-12 rounded-full bg-gray-700/50 place-items-center'>
               <FaStar className='text-orange-400' />
           </div>
         </div>
         <div className='space-y-2'>
-          <motion.h2 initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1}} className='text-2xl md:text-3xl text-gray-200'>How did we do?</motion.h2>
-          <motion.p  initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1.5}} className='text-md md:text-lg font-light text-gray-400'>Please let us know with your support request. All feedback is appreciated to help us improve our offering. </motion.p>
+          <motion.h2 initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1}} className='text-2xl text-gray-200 md:text-3xl'>How did we do?</motion.h2>
+          <motion.p  initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1.5}} className='font-light text-gray-400 text-md md:text-lg'>Please let us know with your support request. All feedback is appreciated to help us improve our offering. </motion.p>
         </div>
         {/* rating */}
         <div className="flex items-center justify-between">
@@ -43,29 +43,29 @@ export default function App() {
           ))}
         </div>
         <div>
-          <button onClick={handleSubmit} type="button" disabled={rating === null} className='disabled:opacity-50 w-full py-3 px-4 rounded-full bg-orange-400 text-center text-gray-200 font-medium uppercase'>Submit</button>
+          <button onClick={handleSubmit} type="button" disabled={rating === null} className='w-full px-4 py-3 font-medium text-center text-gray-200 uppercase bg-orange-400 rounded-full disabled:opacity-50'>Submit</button>
         </div>
       </div>
       ) : (
-        <div className="max-w-sm w-full h-fit py-7 rounded-2xl px-6 space-y-6 bg-gradient-to-t from-gray-900 to-gray-800 flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center justify-center w-full max-w-sm px-6 space-y-6 h-fit py-7 rounded-2xl bg-gradient-to-t from-gray-900 to-gray-800">
           <motion.div initial={{ opacity: 0, scale: .5 }} animate={{ opacity: 1, scale: 1}} transition={{ type: 'tween' }}>
             {rating < 3 && <img className='h-24 w-28' src="/doctor.png" alt="" />}
             {rating === 3 && <img className='h-24 w-28' src="/girl-researching.png" alt="" />}
             {rating >= 4 && <img className='h-24 w-28' src="/thumbs-up.png" alt="" />}
           </motion.div>
-          <div className='bg-gradient-to-t from-gray-800 to-gray-700 w-fit rounded-full py-1 px-3'>
+          <div className='px-3 py-1 rounded-full bg-gradient-to-t from-gray-800 to-gray-700 w-fit'>
             <p className='text-orange-400'>You selected <span className="font-medium text-gray-200">{rating}</span> out of 5.</p>
           </div>
-          <div className='text-center space-y-2'>
-            <motion.p initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1}} className='text-gray-200 text-xl md:text-2xl'>
+          <div className='space-y-2 text-center'>
+            <motion.p initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1}} className='text-xl text-gray-200 md:text-2xl'>
               {rating < 3 &&  <span>We'll work on it!</span>}
               {rating === 3 &&  <span>We can do better!</span>}
               {rating === 4 &&  <span>Awesome!</span>}
               {rating === 5 &&  <span>Perfect!</span>}
             </motion.p>
-            <motion.p initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1.5 }} className='text-gray-400 font-light text-md'>We appreciate you taking the time to give is a rating. If you ever need more support, don't hesitate to get in touch.</motion.p>
+            <motion.p initial={{ opacity: 0, x: -5 }} animate={{ opacity: 1, x: 0}} transition={{ type: 'tween', duration: 1.5 }} className='font-light text-gray-400 text-md'>We appreciate you taking the time to give is a rating. If you ever need more support, don't hesitate to get in touch.</motion.p>
           </div>
-          <button onClick={handleRestart} className='py-2 px-4 rounded-full text-orange-400/50 text-center'>Restart</button>
+          <button onClick={handleRestart} className='px-4 py-2 text-center rounded-full text-orange-400/50'>Restart</button>
         </div>
       )}
     </div>
